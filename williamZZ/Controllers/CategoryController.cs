29edec96f477ced5820f2e837babf9cb884c2d18
william chem <william19022zz@gmail.com>
2022-04-17 23:02:16 +0800

    Initial commit

---
+++ williamZZ/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using williamZZ.Data;
+using williamZZ.Models;
+
+namespace williamZZ.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDBContext _db;
+
+    public CategoryController(ApplicationDBContext db)
+    {
+        _db = db;
+    }
+    public IActionResult Index()
+    {
+        IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+        return View(objCategoryList);
+    }
+
+    //GET
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+}
---
+++ williamZZ/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using williamZZ.Models;
+
+namespace williamZZ.Data;
+
+public class ApplicationDBContext : DbContext
+{
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
